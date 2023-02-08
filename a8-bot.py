547654a4fa82e9d4911f5d661f71158b3b849fe9
sam-astro <77079540+sam-astro@users.noreply.github.com>
2023-02-08 11:28:22 -0500

    Update a8-bot.py

--- a8-bot.py
+++ a8-bot.py
@@ -5,6 +5,18 @@ import uuid
 import discord
 from discord.ext import commands
 
+import re
+ 
+ 
+def FindURL(strIn):
+ 
+    # findall() has been used
+    # with valid conditions for urls in string
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex, strIn)
+    return [x[0] for x in url]
+ 
+
 
 token = os.getenv("DISCORD_TOKEN")
 my_guild = os.getenv("DISCORD_GUILD")
@@ -51,6 +63,9 @@ async def on_message(message):
 
     message_content = message.content.lower()
     codeContent = "".join(message_content.split('\n')[1:])
+    url = ""
+    if len(FindURL(codeContent)) != 0:
+        url = FindURL(codeContent)[0]
     if "/astro8" in message_content:
         # Make sure the user isn't trying to use image only mode, because this is necessary for the bot's functionality
         if "--imagemode" in message_content:
